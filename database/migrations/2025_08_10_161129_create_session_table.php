47eb73be2e10552cf75fd1bd50717312e8f9cb79
mh3yad <mh3yad@gmail.com>
2025-08-10 19:55:13 +0300

    LMS-1 add migration

---
+++ database/migrations/2025_08_10_161129_create_session_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('session', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('course_id')->constrained('course')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('title');
+            $table->dateTime('scheduled_at');
+            $table->integer('duration_minutes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('session');
+    }
+};

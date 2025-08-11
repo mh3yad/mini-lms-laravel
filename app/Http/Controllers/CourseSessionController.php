9e12ca4b1d04bff595acf8145d7febb406d941b6
mh3yad <mh3yad@gmail.com>
2025-08-11 08:49:30 +0300

    LMS-2 add models,controllers, requests, routes

---
+++ app/Http/Controllers/CourseSessionController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCourseSessionRequest;
+use App\Http\Requests\UpdateCourseSessionRequest;
+use App\Models\CourseSession;
+
+class CourseSessionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCourseSessionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CourseSession $courseSession)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CourseSession $courseSession)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCourseSessionRequest $request, CourseSession $courseSession)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CourseSession $courseSession)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\CourseSessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('courses', CourseController::class);
+Route::resource('session', CourseSessionController::class);
